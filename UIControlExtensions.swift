e839329dcdaa1e614246509574aac5c73caaeb37
Taro Minowa Higepon <higepon@gmail.com>
2014-06-10 06:06:27 -0700

    Added UIControl.

---
+++ UIControlExtensions.swift
@@ -0,0 +1,18 @@
+//
+//  UIKitExtensions.swift
+//
+//  Created by Taro Minowa on 6/10/14.
+//  Copyright (c) 2014 Higepon Taro Minowa. All rights reserved.
+//
+
+import UIKit
+
+extension UIControl {
+    func disable() {
+        enabled = false
+    }
+
+    func enable() {
+        enabled = true
+    }
+}
